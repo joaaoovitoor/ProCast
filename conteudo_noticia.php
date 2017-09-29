68f90cb27c6ef96c3cbafdb3c1450748bfd22b7d
mirielesilverio <32047255+mirielesilverio@users.noreply.github.com>
2017-09-29 16:19:50 -0300

    include do head conteudo_notícias

--- conteudo_noticia.php
+++ conteudo_noticia.php
@@ -1,12 +1,9 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 	<head>
-		<meta charset="UTF-8">
-		<title>ProCast</title>
-		<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-	    <link href="css/bootstrap.min.css" rel="stylesheet">
-	    <link rel="stylesheet" href="css/rodape.css">
+		<?php
+            include('link_head.html');
+        ?>
         <link rel="stylesheet" href="css/noticias_cont.css">
 	</head>
 	<body>
