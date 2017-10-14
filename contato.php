d484d0a54d68bd974ada55b4fdd0cc4a955a7621
HerbertGM <herbert-2015-herbert@outlookcom>
2017-10-14 13:55:35 -0300

    30 to 50 img_contato
    aumento campo img

--- contato.php
+++ contato.php
@@ -1,3 +1,6 @@
+<?php
+	include('verificar_logado.php');
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 	<head>
@@ -32,7 +35,7 @@
 						<div class="panel-body">
 							<form action="#" method="POST">
 								<div class="col-md-6">
-									<input type="text" class="form-control input-lg" placeholder="Título da mensagem">
+									<input type="text" name="motivo" class="form-control input-lg" placeholder="Título da mensagem">
 								</div>
 								<div class="col-md-6">
 									<select name="assunto" class="form-control input-lg">
@@ -44,12 +47,12 @@
 									</select>
 								</div>
 								<div class="col-md-12 mg_tp">
-									<textarea name="mensagem" rows="10" maxlength="1000" class="form-control" placeholder="Escreva sua mensagem"></textarea>
+									<textarea name="descricao_contato" rows="10" maxlength="1000" class="form-control" placeholder="Escreva sua mensagem"></textarea>
 								</div>
 								<div class="col-md-12">
 									<p class="help-block mg_tp">Envie um print se necessário</p>
 									<label for='selecao-arquivo'><span class="glyphicon glyphicon-paperclip" aria-hidden="true"></span> Selecionar um arquivo </label>
-									<input id='selecao-arquivo' type='file'>
+									<input id='selecao-arquivo' type='file' name="print">
 								</div>
 								<div class="col-md-12 mg_tp">
 									<button type="submit" class="btn btn-block bg_azul_escuro btn-lg fonte_branca" name="enviar" >Enviar </button>
@@ -60,6 +63,43 @@
 				</div>
 			</div>
 		</div>
+		<?php
+			include('conexao.php');
+			if(isset($_POST['enviar']))
+			{
+				$motivo=$_POST['motivo'];
+				$assunto=$_POST['assunto'];
+				$descricao_contato=$_POST['descricao_contato'];
+				if(isset($_FILES['print']))
+				{
+					if(empty ($motivo) or ($assunto) or ($descricao))
+					{
+						echo ('<script>window.alert("Complete todos os campos");</script>');
+					}
+					else
+					{
+						$sqlin=('INSERT INTO contato(assunto,motivo,descricao_contato,id_usuario) VALUES("'.$assunto.'","'.$motivo.'","'.$descricao.'",'.$con['id_usuario'].');');
+						$inserir=mysql_query($conexao,$sqlin);
+					}
+				}
+				else
+				{
+					$extensao=strtolower(substr($_FILES['anexo']['name'], -4));
+	                $novo_nome=md5(time().$con['id_usuario']).$extensao;
+	                $diretorio="uploads/";
+	                move_uploaded_file($_FILES['anexo']['tmp_name'], $diretorio.$novo_nome);
+	                if(empty ($motivo) or ($assunto) or ($descricao))
+	                {
+						echo ('<script>window.alert("Complete todos os campos");</script>');
+					}
+					else
+					{
+						$sqlin=('INSERT INTO contato(assunto,motivo,descricao_contato,imagem_contato,id_usuario) VALUES("'.$assunto.'","'.$motivo.'","'.$descricao.'","'.$novo_nome.'",'.$con['id_usuario'].');');
+						$inserir=mysql_query($conexao,$sqlin);
+					}
+				}
+			}
+		?>
 		<?php
 			include('rodape.html');
 		?>
