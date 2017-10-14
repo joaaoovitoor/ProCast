fe6b54c215aa1ec102bf491a95c43c63b51b41d9
João Vitor <32064287+negaorx@users.noreply.github.com>
2017-10-14 00:45:17 -0300

    Cadastro de clube com logo
    Contém bug em cadastrar sem o logo

--- criar_clube.php
+++ criar_clube.php
@@ -1,3 +1,6 @@
+<?php 
+	include('verificar_logado.php');
+?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
 	<head>
@@ -26,7 +29,7 @@
                 </div>
             </div>
         </div>
-        <form action="#" method="POST">
+        <form action="#" method="POST" enctype="multipart/form-data">
 			<div class="container-fluid">
 				<div class="row">
 					<div class="col-md-offset-1 col-md-8 col-sm-offset-1 col-sm-8 col-xs-offset-1 col-xs-6">
@@ -36,12 +39,12 @@
 						</div>
 						<div class="form-group">
 			                Descrição
-			                <textarea class="form-control" rows="3" placeholder="Descreva sobre seu clube" name="descricao"></textarea>
+			                <textarea class="form-control" rows="6" placeholder="Descreva sobre seu clube" name="descricao"></textarea>
 			            </div>
 					</div>
 					<div class="col-md-3 col-sm-3">
 							<label for='selecao-arquivo'><span class="glyphicon glyphicon-paperclip" aria-hidden="true"></span> Importar Logo </label>
-							<input id='selecao-arquivo' type='file'>
+							<input id='selecao-arquivo' type='file' name="anexo">
 					</div>
 				</div>
 			</div>
@@ -61,25 +64,42 @@
 			{
 				$nome = $_POST['nome'];
 				$descricao = $_POST['descricao'];
-				$logo_clube = $_POST['logo_clube'];
-
-				if(empty($nome) or empty($descricao))
+				if(!isset($_FILES['anexo']))
 				{
-					echo ('<script>window.alert("Digite todos os dados");</script>');
-				}
-				else
-				{
-					$sqlin=('INSERT INTO clube(logo_clube,nome_clube,dta_cricao,descricao_clube,id_usuario) VALUES("'.$logo_clube.'","'.$nome.'",NOW(),"'.$descricao.'",'.$_SESSION['id_usuario'].';');
-					$inserir=mysqli_query($conexao,$sqlin);
-
-					if($inserir)
+					if(empty($nome) or empty($descricao))
 					{
-						echo ('<script>window.alert("Clube criado com sucesso!");window.location.href="clube_investidor.php";</script>');
+						echo ('<script>window.alert("Digite todos os dados");</script>');
 					}
 					else
 					{
-						echo ('<script>window.alert("Erro ao criar Clube!");window.location.href="criar_clube.php";</script>');
+						$sqlin=('INSERT INTO clube(nome_clube,dta_criacao,descricao_clube,id_usuario) VALUES("'.$nome.'",NOW(),"'.$descricao.'",'.$con['id_usuario'].');');
+						$inserir=mysqli_query($conexao,$sqlin);
+						$teste="Sem imagem";
 					}
+				}
+				else{
+	                $extensao=strtolower(substr($_FILES['anexo']['name'], -4));
+	                $novo_nome=md5(time().$con['id_usuario']).$extensao;
+	                $diretorio="uploads/";
+	                move_uploaded_file($_FILES['anexo']['tmp_name'], $diretorio.$novo_nome);
+					if(empty($nome) or empty($descricao))
+					{
+						echo ('<script>window.alert("Digite todos os dados");</script>');
+					}
+					else
+					{
+						$sqlin=('INSERT INTO clube(logo_clube,nome_clube,dta_criacao,descricao_clube,id_usuario) VALUES("'.$novo_nome.'","'.$nome.'",NOW(),"'.$descricao.'",'.$con['id_usuario'].');');
+						$inserir=mysqli_query($conexao,$sqlin);
+						$teste="Com img";
+					}
+				}
+				if($inserir)
+				{
+					echo ('<script>window.alert("Clube criado com sucesso!'.$teste.'");window.location.href="clube_investidor.php";</script>');
+				}
+				else
+				{
+					echo ('<script>window.alert("Erro ao criar Clube!'.$teste.'");window.location.href="criar_clube.php";</script>');
 				}
 			}
 		?>
