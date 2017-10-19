abd086866c57800bd0659ce0e032db461522d02c
João Vitor <32064287+negaorx@users.noreply.github.com>
2017-10-19 19:21:59 -0200

    API com url_encode

--- teste_url.php
+++ teste_url.php
@@ -1,9 +1,11 @@
 <?php
-	$nome = 'xNeg%C3%A3o%20RX';
 	
-	$urljogo = file_get_contents("https://br1.api.riotgames.com/lol/summoner/v3/summoners/by-name/$nome?api_key=RGAPI-d89b2cfe-8b5f-4f55-80ef-abc61bc8d931");
+	$apikey="RGAPI-52ab3dd2-16d3-404c-bdc5-14279ce28ec6";
+	$nick ="xNegão RX";
+	$nickcod = rawurlencode($nick);
+	echo $nickcod;
 	
-	//echo 'Resultado: '.$urljogo;
+	$urljogo = file_get_contents("https://br1.api.riotgames.com/lol/summoner/v3/summoners/by-name/$nickcod?api_key=$apikey");
 	
 	$dados = json_decode($urljogo);
 
@@ -11,17 +13,9 @@
 
 	$id=$dados->id;
 
-	/*$idinteira = explode(':',$urljogo);
-	
-	$id = explode(',',$idinteira[1]);
-	
-	echo '<br>';
-	
-	echo 'id> '.$id[0];*/
+	$urlrank = file_get_contents("https://br1.api.riotgames.com/lol/league/v3/positions/by-summoner/$id?api_key=$apikey");
 
-
-	$urlrank = file_get_contents("https://br1.api.riotgames.com/lol/league/v3/positions/by-summoner/$id?api_key=RGAPI-d89b2cfe-8b5f-4f55-80ef-abc61bc8d931");
-
+	//solo/duo
 	$filas = explode('},',$urlrank);
 	$filasolo = $filas[0].'}';
 	$solo = str_replace("[","", $filasolo);
@@ -31,6 +25,7 @@
 	echo '<br>Tier: '.$rankings->tier;
 	echo '<br>Rank: '.$rankings->rank;
 
+	//flex
 	$filaflex =$filas[1];
 	$flex = str_replace("]","", $filaflex);
 	$rankingf = json_decode($flex);
@@ -38,17 +33,4 @@
 	echo "<br><br><br>Fila Flex";
 	echo '<br>Tier: '.$rankingf->tier;
 	echo '<br>Rank: '.$rankingf->rank;
-	/*
-
-	$posicao = explode(',',$urlrank);
-	
-	$tier1 = explode(':',$posicao[2]);
-	
-	$rank1 = explode(':',$posicao[4]);
-
-	echo "<br>Resultado> rank".$urlrank;
-
-	echo "<br>Resultado tier".$tier1[1];
-
-	echo "<br>Resultado rank".$rank1[1];*/
 ?>
