76e5a6d589bab443b02250afc48d9a83203f06ec
João Vitor <32064287+negaorx@users.noreply.github.com>
2017-10-18 23:10:47 -0200

    Ajustes API

--- teste_url.php
+++ teste_url.php
@@ -1,22 +1,46 @@
 <?php
 	$nome = 'xNeg%C3%A3o%20RX';
 	
-	
-	
 	$urljogo = file_get_contents("https://br1.api.riotgames.com/lol/summoner/v3/summoners/by-name/$nome?api_key=RGAPI-d89b2cfe-8b5f-4f55-80ef-abc61bc8d931");
 	
-	echo 'Resultado: '.$urljogo;
+	//echo 'Resultado: '.$urljogo;
 	
-	$idinteira = explode(':',$urljogo);
+	$dados = json_decode($urljogo);
+
+	echo '<br>ID: '.$dados->id;
+
+	$id=$dados->id;
+
+	/*$idinteira = explode(':',$urljogo);
 	
 	$id = explode(',',$idinteira[1]);
 	
 	echo '<br>';
 	
-	echo 'id> '.$id[0];
+	echo 'id> '.$id[0];*/
 
 
-	$urlrank = file_get_contents("https://br1.api.riotgames.com/lol/league/v3/positions/by-summoner/$id[0]?api_key=RGAPI-d89b2cfe-8b5f-4f55-80ef-abc61bc8d931");
+	$urlrank = file_get_contents("https://br1.api.riotgames.com/lol/league/v3/positions/by-summoner/$id?api_key=RGAPI-d89b2cfe-8b5f-4f55-80ef-abc61bc8d931");
+
+	$filas = explode('},',$urlrank);
+	$filasolo = $filas[0].'}]';
+	$retirar = str_replace("[","", $filasolo);
+	$solo = str_replace("]","", $retirar);
+	$rankings = json_decode($solo);
+
+	echo "<br><br><br>Fila Solo/Duo";
+	echo '<br>Tier: '.$rankings->tier;
+	echo '<br>Rank: '.$rankings->rank;
+
+	$filaflex = "[".$filas[1];
+	$retirar = str_replace("[","", $filaflex);
+	$filaflex = str_replace("]","", $retirar);
+	$rankingf = json_decode($filaflex);
+
+	echo "<br><br><br>Fila Flex";
+	echo '<br>Tier: '.$rankingf->tier;
+	echo '<br>Rank: '.$rankingf->rank;
+	/*
 
 	$posicao = explode(',',$urlrank);
 	
@@ -28,5 +52,5 @@
 
 	echo "<br>Resultado tier".$tier1[1];
 
-	echo "<br>Resultado rank".$rank1[1];
+	echo "<br>Resultado rank".$rank1[1];*/
 ?>
