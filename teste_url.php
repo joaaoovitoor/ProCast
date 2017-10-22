32978a992c02cdd3cc2f9359370cc84808cd7604
João Vitor <32064287+negaorx@users.noreply.github.com>
2017-10-22 21:23:38 -0200

    update again

--- teste_url.php
+++ teste_url.php
@@ -1,52 +1,31 @@
 <?php
 	
 	$apikey="RGAPI-f8999a64-bec3-4b2d-acd3-1ab70789db9e";
-	$nick ='xNegão RX';
-	//$nick ='teste';
-	//$nick ='fhgdfkdjkf';
+	$urlrank = file_get_contents('https://br1.api.riotgames.com/lol/league/v3/positions/by-summoner/'.$con['id_nick'].'?api_key='.$apikey.'');
 	
-	$nickcod = rawurlencode($nick);
-	$urljogo = @file_get_contents("https://br1.api.riotgames.com/lol/summoner/v3/summoners/by-name/$nickcod?api_key=$apikey");
-	
-	if($urljogo){
+	//solo/duo
+	$filas = explode('},',$urlrank);
+	$filasolo = $filas[0].'}';
+	$solo = str_replace("[","", $filasolo);
+	$rankings = json_decode($solo);
 
-		$retirar = array('{','"','}');
-		$urljogo = str_replace($retirar, '', $urljogo);
-		$urljogo = str_replace(',', ':', $urljogo);
-		$urlid = explode(':',$urljogo);
-		
+	if ($rankings->rank){
+		echo "<br><br><br>Fila Solo/Duo";
+		echo '<br>Tier: '.$rankings->tier;
+		echo '<br>Rank: '.$rankings->rank;
 
-		$urlrank = file_get_contents("https://br1.api.riotgames.com/lol/league/v3/positions/by-summoner/$urlid[1]?api_key=$apikey");
-		
-		//solo/duo
-		$filas = explode('},',$urlrank);
-		$filasolo = $filas[0].'}';
-		$solo = str_replace("[","", $filasolo);
-		$rankings = json_decode($solo);
+		//flex
+		$filaflex =$filas[1];
+		$flex = str_replace("]","", $filaflex);
+		$rankingf = json_decode($flex);
 
-		if ($rankings->rank){
-			echo "<br><br><br>Fila Solo/Duo";
-			echo '<br>Tier: '.$rankings->tier;
-			echo '<br>Rank: '.$rankings->rank;
-
-			//flex
-			$filaflex =$filas[1];
-			$flex = str_replace("]","", $filaflex);
-			$rankingf = json_decode($flex);
-
-			echo "<br><br><br>Fila Flex";
-			echo '<br>Tier: '.$rankingf->tier;
-			echo '<br>Rank: '.$rankingf->rank;
-		}
-		else
-		{
-			echo ('usuario não possui rank');
-		}
-		
+		echo "<br><br><br>Fila Flex";
+		echo '<br>Tier: '.$rankingf->tier;
+		echo '<br>Rank: '.$rankingf->rank;
 	}
 	else
 	{
-		echo "Jogador inexistente";
-	}	
+		echo ('usuario não possui rank');
+	}
 
 ?>
