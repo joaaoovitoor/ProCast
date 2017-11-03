b90e05efe39b47023ea33954d48c45703dd4fab0
João Vitor <32064287+negaorx@users.noreply.github.com>
2017-11-03 13:56:05 -0200

    verificações

--- moderar_anuncios.php
+++ moderar_anuncios.php
@@ -1,11 +1,23 @@
 <?php
+    ob_start();
     session_start();
     if(isset($_SESSION['email'])){
         $email_usuario=$_SESSION['email'];
         include('conexao.php');
-        $sqlsel='select * from anunciante where email="'.$email_usuario.'";';
+        //verificando se ele é usuário
+        $sqlsel='select * from usuario where email="'.$email_usuario.'";';
         $resul=mysqli_query($conexao,$sqlsel);
-        $con=mysqli_fetch_array($resul);
+        if(mysqli_num_rows($resul)>0)
+        {
+        	header("location: destruir.php");
+        }
+        //se ele realmente for anunciante, cairá neste else
+        else
+        {
+	        $sqlsel='select * from anunciante where email="'.$email_usuario.'";';
+	        $resul=mysqli_query($conexao,$sqlsel);
+	        $con=mysqli_fetch_array($resul);
+	    }
     }
     else{
         header('location:destruir.php');    
--- verificar_logado.php
+++ verificar_logado.php
@@ -4,9 +4,21 @@
     if(isset($_SESSION['email'])){
         $email_usuario=$_SESSION['email'];
         include('conexao.php');
-        $sqlsel='select * from usuario where email="'.$email_usuario.'";';
+        //verificando se ele é anunciante
+        $sqlsel='select * from anunciante where email="'.$email_usuario.'";';
         $resul=mysqli_query($conexao,$sqlsel);
-        $con=mysqli_fetch_array($resul);
+        if(mysqli_num_rows($resul)>0)
+        {
+            header("location: destruir.php");
+        }
+        //se ele realmente for anunciante, cairá neste else
+        else
+        { 
+            $con=mysqli_fetch_array($resul);
+            $sqlsel='select * from usuario where email="'.$email_usuario.'";';
+            $resul=mysqli_query($conexao,$sqlsel);
+            $con=mysqli_fetch_array($resul);
+        }
     }
     else{
         header('location:destruir.php');    
