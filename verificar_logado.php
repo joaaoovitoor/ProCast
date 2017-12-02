8639663f4af26ab016ea018cdf3756bbdf723b0e
João Vitor <32064287+negaorx@users.noreply.github.com>
2017-12-02 01:32:34 -0200

    bloquei de acesso admin

--- verificar_logado.php
+++ verificar_logado.php
@@ -7,11 +7,22 @@
         //verificando se ele é anunciante
         $sqlsel='select * from anunciante where email="'.$email_usuario.'";';
         $resul=mysqli_query($conexao,$sqlsel);
+
+        //verificando se ele é admin
+        $sqlsela='select * from admin where email="'.$email_usuario.'";';
+        $resula=mysqli_query($conexao,$sqlsela);
+
+        //se ele for anunciante, cairá aqui
         if(mysqli_num_rows($resul)>0)
         {
             header("location: perfil_anunciante.php");
         }
-        //se ele realmente for anunciante, cairá neste else
+        //se ele for admin, cairá aqui
+        elseif(mysqli_num_rows($resula)>0)
+        {
+            header("location: admin.php");
+        }
+        //se ele realmente for usuario, cairá neste else
         else
         { 
             $sqlsel='select * from usuario where email="'.$email_usuario.'";';
