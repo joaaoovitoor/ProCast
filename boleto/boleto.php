de89e3af86e9480be7dfe40dca33463a3c8b8a2f
João Vitor <32064287+negaorx@users.noreply.github.com>
2017-12-02 14:46:19 -0200

    boleto para o usuário

---
+++ boleto/boleto.php
@@ -0,0 +1,97 @@
+<?php
+// +----------------------------------------------------------------------+
+// | BoletoPhp - Versão Beta                                              |
+// +----------------------------------------------------------------------+
+// | Este arquivo está disponível sob a Licença GPL disponível pela Web   |
+// | em http://pt.wikipedia.org/wiki/GNU_General_Public_License           |
+// | Você deve ter recebido uma cópia da GNU Public License junto com     |
+// | esse pacote; se não, escreva para:                                   |
+// |                                                                      |
+// | Free Software Foundation, Inc.                                       |
+// | 59 Temple Place - Suite 330                                          |
+// | Boston, MA 02111-1307, USA.                                          |
+// +----------------------------------------------------------------------+
+// +----------------------------------------------------------------------+
+// | Originado do Projeto BBBoletoFree que tiveram colaborações de Daniel |
+// | William Schultz e Leandro Maniezo que por sua vez foi derivado do	  |
+// | PHPBoleto de João Prado Maia e Pablo Martins F. Costa				        |
+// | 														                                   			  |
+// | Se vc quer colaborar, nos ajude a desenvolver p/ os demais bancos :-)|
+// | Acesse o site do Projeto BoletoPhp: www.boletophp.com.br             |
+// +----------------------------------------------------------------------+
+// +----------------------------------------------------------------------+
+// | Equipe Coordenação Projeto BoletoPhp: <boletophp@boletophp.com.br>   |
+// | Desenvolvimento Boleto Itaú: Glauber Portella                        |
+// +----------------------------------------------------------------------+
+// ------------------------- DADOS DINÂMICOS DO SEU CLIENTE PARA A GERAÇÃO DO BOLETO (FIXO OU VIA GET) -------------------- //
+// Os valores abaixo podem ser colocados manualmente ou ajustados p/ formulário c/ POST, GET ou de BD (MySql,Postgre,etc)	//
+// DADOS DO BOLETO PARA O SEU CLIENTE
+
+include('../conexao.php');
+include('../verificar_logado.php');
+if($con['categoria_usuario']==2)
+    {
+    	header('location:boleto_investidor.php');
+    }
+
+//trazendo as informações de localização do cliente
+$sqlest='SELECT nome FROM estado WHERE id='.$con['estado'].';';
+$resulest=mysqli_query($conexao,$sqlest);
+$conest=mysqli_fetch_array($resulest);
+
+$sqlcid='SELECT nome FROM cidade WHERE id='.$con['cidade'].';';
+$resulcid=mysqli_query($conexao,$sqlcid);
+$concid=mysqli_fetch_array($resulcid);
+
+
+//boleto
+$dias_de_prazo_para_pagamento = 5;
+$taxa_boleto = 2.95;
+$data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
+$valor_cobrado = "8,99"; // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
+$valor_cobrado = str_replace(",", ".",$valor_cobrado);
+$valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
+$dadosboleto["nosso_numero"] = '78965147';  // Nosso numero - REGRA: Máximo de 8 caracteres!
+$dadosboleto["numero_documento"] = '4869';	// Num do pedido ou nosso numero
+$dadosboleto["data_vencimento"] = $data_venc; // Data de Vencimento do Boleto - REGRA: Formato DD/MM/AAAA
+$dadosboleto["data_documento"] = date("d/m/Y"); // Data de emissão do Boleto
+$dadosboleto["data_processamento"] = date("d/m/Y"); // Data de processamento do boleto (opcional)
+$dadosboleto["valor_boleto"] = $valor_boleto; 	// Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da virgula
+// DADOS DO SEU CLIENTE
+$dadosboleto["sacado"] = $con['nome']." ".$con['sobrenome'];
+$dadosboleto["endereco1"] = $concid['nome'];
+$dadosboleto["endereco2"] = $conest['nome'];
+// INFORMACOES PARA O CLIENTE
+$dadosboleto["demonstrativo1"] = "Pagamento de Compra na Loja ProCast";
+$dadosboleto["demonstrativo2"] = "Pagamento referente ao cadastro como jogador<br>Taxa bancária - R$ ".number_format($taxa_boleto, 2, ',', '');
+$dadosboleto["demonstrativo3"] = "BoletoPhp - http://www.boletophp.com.br";
+$dadosboleto["instrucoes1"] = "- Sr. Caixa, cobrar multa de 2% após o vencimento";
+$dadosboleto["instrucoes2"] = "- Receber até 10 dias após o vencimento";
+$dadosboleto["instrucoes3"] = "- Em caso de dúvidas entre em contato conosco: contato@procast.com.br";
+$dadosboleto["instrucoes4"] = "&nbsp; Emitido pelo sistema Projeto BoletoPhp - www.boletophp.com.br";
+// DADOS OPCIONAIS DE ACORDO COM O BANCO OU CLIENTE
+$dadosboleto["quantidade"] = "1";
+$dadosboleto["valor_unitario"] = "8,99";
+$dadosboleto["aceite"] = "";		
+$dadosboleto["especie"] = "R$";
+$dadosboleto["especie_doc"] = "";
+// ---------------------- DADOS FIXOS DE CONFIGURAÇÃO DO SEU BOLETO --------------- //
+// DADOS DA SUA CONTA - ITAÚ
+$dadosboleto["agencia"] = "1565"; // Num da agencia, sem digito
+$dadosboleto["conta"] = "13877";	// Num da conta, sem digito
+$dadosboleto["conta_dv"] = "4"; 	// Digito do Num da conta
+// DADOS PERSONALIZADOS - ITAÚ
+$dadosboleto["carteira"] = "175";  // Código da Carteira: pode ser 175, 174, 104, 109, 178, ou 157
+// SEUS DADOS
+$dadosboleto["identificacao"] = "BoletoPhp - Código Aberto de Sistema de Boletos";
+$dadosboleto["cpf_cnpj"] = "46345322806";
+$dadosboleto["endereco"] = "
+Rua Professora Ana Moutinho Gonçalves, 57";
+$dadosboleto["cidade_uf"] = "Santa Isabel / São Paulo";
+$dadosboleto["cedente"] = "ProCast SA";
+// NÃO ALTERAR!
+include("include/funcoes_itau.php"); 
+include("include/layout_itau.php");
+
+$dadosboleto["linha_digitavel"];
+?>
